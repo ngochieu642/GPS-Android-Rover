e3768a28123396772a945f52b76346d4afa354e1
ngochieu642 <ngochieu10041997@gmail.com>
2019-05-02 15:51:38 +0700

    Add time count

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URISyntaxException;
 
 import io.socket.client.IO;
@@ -13,8 +16,8 @@ import io.socket.emitter.Emitter;
 public class MainActivity extends FragmentActivity {
 
     private Socket mSocket;
-//    private static String host_url = "http://192.168.1.12:3000/"; //local host
-    private static String host_url = "https://gps-server-nodejs.herokuapp.com/";
+    private static String host_url = "http://192.168.1.12:3000/"; //local host
+//    private static String host_url = "https://gps-server-nodejs.herokuapp.com/";
     private TextView txtMessage, txtSocketID;
 
     @Override
@@ -24,8 +27,6 @@ public class MainActivity extends FragmentActivity {
 
         txtMessage=findViewById(R.id.txtMessage);
         txtSocketID=findViewById(R.id.txtSocketID);
-
-
     }
 
     @Override
@@ -58,11 +59,20 @@ public class MainActivity extends FragmentActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    String data =(String)args[0]; //Dữ liệu nhận được dạng String
+                    JSONObject object =(JSONObject)args[0];
+                    String data = null; //Dữ liệu nhận được dạng String
+                    int count = 0;
+                    try {
+                        data = object.getString("GPS");
+                        count= object.getInt("count");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     if(data!=null){
                         System.out.println("\n\n\n\nReceive " + data.length()+" bytes from Server");
                         System.out.println(data);
-                        txtMessage.setText(data);
+                        txtMessage.setText(data + "\nTimes: "+Integer.toString(count));
                     }
                 }
             });
